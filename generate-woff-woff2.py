232df7e4f31e070bea1ae58c9f44b8a8af9ed04a
Cristiano Sobral <cssobral2013@gmail.com>
2023-12-13 10:27:54 -0300

    Update

---
+++ generate-woff-woff2.py
@@ -0,0 +1,333 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/KoreeneSans-Thin.ttf'
+woffPath = './web/KoreeneSans-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/KoreeneSans-ThinItalic.ttf'
+woffPath = './web/KoreeneSans-ThinItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/KoreeneSans-ExtraLight.ttf'
+woffPath = './web/KoreeneSans-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/KoreeneSans-ExtraLightItalic.ttf'
+woffPath = './web/KoreeneSans-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/KoreeneSans-Light.ttf'
+woffPath = './web/KoreeneSans-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-LightItalic.ttf'
+woffPath = './web/KoreeneSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-LightItalic.ttf'
+woffPath = './web/KoreeneSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-Regular.ttf'
+woffPath = './web/KoreeneSans-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-Italic.ttf'
+woffPath = './web/KoreeneSans-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-Medium.ttf'
+woffPath = './web/KoreeneSans-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-MediumItalic.ttf'
+woffPath = './web/KoreeneSans-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-SemiBold.ttf'
+woffPath = './web/KoreeneSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-SemiBoldItalic.ttf'
+woffPath = './web/KoreeneSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-SemiBold.ttf'
+woffPath = './web/KoreeneSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-SemiBoldItalic.ttf'
+woffPath = './web/KoreeneSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-Bold.ttf'
+woffPath = './web/KoreeneSans-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-BoldItalic.ttf'
+woffPath = './web/KoreeneSans-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-ExtraBold.ttf'
+woffPath = './web/KoreeneSans-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-ExtraBoldItalic.ttf'
+woffPath = './web/KoreeneSans-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-Black.ttf'
+woffPath = './web/KoreeneSans-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/KoreeneSans-BlackItalic.ttf'
+woffPath = './web/KoreeneSans-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/KoreeneSans-Thin.ttf'
+woff2Path = './web/KoreeneSans-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/KoreeneSans-ThinItalic.ttf'
+woff2Path = './web/KoreeneSans-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/KoreeneSans-ExtraLight.ttf'
+woff2Path = './web/KoreeneSans-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/KoreeneSans-ExtraLightItalic.ttf'
+woff2Path = './web/KoreeneSans-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/KoreeneSans-Light.ttf'
+woff2Path = './web/KoreeneSans-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-LightItalic.ttf'
+woff2Path = './web/KoreeneSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-LightItalic.ttf'
+woff2Path = './web/KoreeneSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-Regular.ttf'
+woff2Path = './web/KoreeneSans-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-Italic.ttf'
+woff2Path = './web/KoreeneSans-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-Medium.ttf'
+woff2Path = './web/KoreeneSans-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-MediumItalic.ttf'
+woff2Path = './web/KoreeneSans-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-SemiBold.ttf'
+woff2Path = './web/KoreeneSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-SemiBoldItalic.ttf'
+woff2Path = './web/KoreeneSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-SemiBold.ttf'
+woff2Path = './web/KoreeneSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-SemiBoldItalic.ttf'
+woff2Path = './web/KoreeneSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-Bold.ttf'
+woff2Path = './web/KoreeneSans-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-BoldItalic.ttf'
+woff2Path = './web/KoreeneSans-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-ExtraBold.ttf'
+woff2Path = './web/KoreeneSans-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-ExtraBoldItalic.ttf'
+woff2Path = './web/KoreeneSans-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-Black.ttf'
+woff2Path = './web/KoreeneSans-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/KoreeneSans-BlackItalic.ttf'
+woff2Path = './web/KoreeneSans-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
